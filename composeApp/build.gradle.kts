441652539d84aa119e04eaeb1669f9ecc2757eea
Muhamad Abdul Aziz <azizgimboell@gmail.com>
2025-06-29 17:20:02 +0700

    build: Update Android app version to 2.0.0
    The `versionCode` has been incremented from 1 to 2, and the `versionName` has been updated from "1.0.0" to "2.0.0".

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -96,8 +96,8 @@ android {
         targetSdk = 35
 
         applicationId = "dev.abdl.tryfirst.androidApp"
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
